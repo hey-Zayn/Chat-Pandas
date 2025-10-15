3640c1dabc2d40e2cbd9af470ad54566ece26f96
hey-Zayn <zaynobusiness@gmail.com>
2025-10-15 19:57:30 +0500

    CP-02

--- src/app/Contact/page.jsx
+++ src/app/Contact/page.jsx
@@ -7,7 +7,7 @@ const Page = () => {
   return (
     <div className="w-full h-full bg-[#191919] ">
       <div className="w-full h-full pt-30">
-        {/* <Marquee /> */}
+        <Marquee />
         <div className="text-white w-full h-full pb-[2%] overflow-hidden bg-[#181818] pt-15">
       
           <div className="flex flex-col sm:flex-col md:flex-row xl:flex-row  lg:flex-row 2xl:flex-row gap-[1%] justify-between">
